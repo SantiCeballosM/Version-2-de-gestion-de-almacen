3e551a4d1b23b3dfa84c324d2529003fcb5b5cc0
SantiCeballosM <107371068+SantiCeballosM@users.noreply.github.com>
2024-05-14 23:14:04 -0500

    Avance Menu Almacenero

--- src/main/java/Gestion_Almacen/MenuAlmacenero.java
+++ src/main/java/Gestion_Almacen/MenuAlmacenero.java
@@ -1,5 +1,21 @@
 package Gestion_Almacen;
 
-public class MenuAlmacenero {
+import java.util.Scanner;
 
+public class MenuAlmacenero {
+    public static void main (String[] args){
+        //MenuPrincipal.main(new String[0]);
+            Scanner scanner = new Scanner(System.in);
+            int opci;
+            do{
+                System.out.println("MENU ALMACENERO");
+                System.out.println();
+                System.out.println("1- Ver inventario de productos");
+                System.out.println("2- Gestionar pedidos");
+                System.out.println("3- Confirmar envios");
+                System.out.println("4- Salir del sistema");
+                opci = scanner. nextInt();
+
+            }while(opci != 4);
+    }
 }

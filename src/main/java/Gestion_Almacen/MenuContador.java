31f47df492c4c0a35772ce12c931b35a6ac2d5cd
SantiCeballosM <107371068+SantiCeballosM@users.noreply.github.com>
2024-05-14 23:38:49 -0500

    Avance menu Contador

--- src/main/java/Gestion_Almacen/MenuContador.java
+++ src/main/java/Gestion_Almacen/MenuContador.java
@@ -1,4 +1,20 @@
 package Gestion_Almacen;
 
+import java.util.Scanner;
+
 public class MenuContador {
+    public static void main (String[] args){
+        //MenuPrincipal.main(new String[0]);
+        Scanner scanner = new Scanner(System.in);
+        int opci;
+        do{
+            System.out.println("MENU ALMACENERO");
+            System.out.println();
+            System.out.println("1- Ver inventario de productos");
+            System.out.println("2- Generar informe de inventario");
+            System.out.println("3- Salir del sistema");
+            opci = scanner. nextInt();
+
+        }while(opci != 3);
+    }
 }

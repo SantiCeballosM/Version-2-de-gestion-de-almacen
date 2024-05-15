6493e842c6078aeb19ec4e946d75a6c7cd39ab85
SantiCeballosM <107371068+SantiCeballosM@users.noreply.github.com>
2024-05-14 23:30:20 -0500

    Avance Menu Administrador

--- src/main/java/Gestion_Almacen/MenuAdministrador.java
+++ src/main/java/Gestion_Almacen/MenuAdministrador.java
@@ -1,4 +1,26 @@
 package Gestion_Almacen;
-public class MenuAdministrador {
 
+import java.util.Scanner;
+
+public class MenuAdministrador {
+    public static void main (String[] args){
+        //MenuPrincipal.main(new String[0]);
+        Scanner scanner = new Scanner(System.in);
+        int opci;
+        do{
+            System.out.println("MENU ADMINISTRADOR");
+            System.out.println();
+            System.out.println("1- Agregar Producto");
+            System.out.println("2- Modificar producto");
+            System.out.println("3- Eliminar Producto");
+            System.out.println("4- Gestionar Envios");
+            System.out.println("5- Confirmar envios");
+            System.out.println("6- Generar informe de venta");
+            System.out.println("7- Generar informe de inventario");
+            System.out.println("8- Administrar roles y asignar roles");
+            System.out.println("9- Salir del sistema");
+            opci = scanner. nextInt();
+
+        }while(opci != 9);
+    }
 }

34997060d185918d8779f82d6c11c3b5dbcb606a
SantiCeballosM <107371068+SantiCeballosM@users.noreply.github.com>
2024-05-21 22:47:54 -0500

    Adelanto de menu Almacenero

--- src/main/java/Gestion_Almacen/MenuAlmacenero.java
+++ src/main/java/Gestion_Almacen/MenuAlmacenero.java
@@ -1,11 +1,14 @@
 package Gestion_Almacen;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class MenuAlmacenero {
 
     public static void main (String[] args){
         Usuario almaceUsuario = new Usuario();
+        Informe informe = new Informe();
+        Pedido pedido = new Pedido(1, new Date());
         Scanner scanner = new Scanner(System.in);
         boolean loggedIn = false;
         while (!loggedIn) {
@@ -21,14 +24,43 @@ public class MenuAlmacenero {
                     System.out.println("MENU ALMACENERO");
                     System.out.println();
                     System.out.println("1- Ver inventario de productos");
-                    System.out.println("2- Gestionar pedidos");
-                    System.out.println("3- Confirmar envios");
-                    System.out.println("4- Salir del sistema");
+                    System.out.println("2- Agregar Pedido");
+                    System.out.println("3- Modificar Pedido");
+                    System.out.println("4- Eliminar Pedido");
+                    System.out.println("5- Mostrar Pedido");
+                    System.out.println("6- Salir del sistema");
                     System.out.println();
                     System.out.println("Ingresa la opcion: ");
                     opci = scanner.nextInt();
-
-                } while (opci != 4);
+                    System.out.println();
+                    switch (opci) {
+                        case 1:
+                            informe.generarInformeInventario();
+                            System.out.println();
+                            break;
+                        case 2:
+                            Pedido.gestionarPedido(pedido, scanner);
+                            break;
+                        case 3:
+                            Pedido.mostrarPedidos();
+                            Pedido.modificarPedido(pedido, scanner);
+                            break;
+                        case 4:
+                            System.out.println("Pedidos actuales:");
+                            Pedido.mostrarPedidos();
+                            Pedido.eliminarPedido(scanner);
+                            break;
+                        case 5:
+                            Pedido.mostrarPedidos();
+                            break;
+                        case 6:
+                            System.out.println("Saliste del sistema");
+                            break;
+                        default:
+                            System.out.println("Ups opcion invalida");
+                            break;
+                    }
+                } while (opci != 6);
 
                 loggedIn = true;
             } else {
--- src/main/java/Gestion_Almacen/MenuContador.java
+++ src/main/java/Gestion_Almacen/MenuContador.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MenuContador {
     public static void main (String[] args){
         Usuario contUsuario = new Usuario();
+        Informe informe = new Informe();
         Scanner scanner = new Scanner(System.in);
         boolean loggedIn = false;
         while (!loggedIn) {
@@ -20,13 +21,21 @@ public class MenuContador {
                     System.out.println("MENU CONTADOR");
                     System.out.println();
                     System.out.println("1- Ver inventario de productos");
-                    System.out.println("2- Generar informe de inventario");
-                    System.out.println("3- Salir del sistema");
+                    System.out.println("2- Ver inventario de ventas");
+                    System.out.println("2- Salir del sistema");
                     System.out.println();
                     System.out.println("Ingresa la opcion: ");
                     opci = scanner. nextInt();
-
-                }while(opci != 3);
+                    System.out.println();
+                    switch (opci) {
+                        case 1:
+                            informe.generarInformeInventario();
+                            break;
+                        default:
+                            System.out.println("Ups opcion invalida");
+                            break;
+                    }
+                }while(opci != 2);
                 loggedIn = true;
             } else {
                 System.out.println("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
